d9ac4b10542ba3919c0586655d094455075c2abe
Andy258852 <62135208+Andy258852@users.noreply.github.com>
2020-10-09 00:10:12 +0300

    Add files via upload

---
+++ FileManager/FileManager/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.archiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unzipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(0, 26);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(697, 421);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(0);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(697, 26);
+            this.menuStrip1.Stretch = false;
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.AutoSize = false;
+            this.toolStripMenuItem1.BackColor = System.Drawing.Color.White;
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.archiveToolStripMenuItem,
+            this.unzipToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Padding = new System.Windows.Forms.Padding(0);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(40, 26);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // archiveToolStripMenuItem
+            // 
+            this.archiveToolStripMenuItem.Name = "archiveToolStripMenuItem";
+            this.archiveToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.archiveToolStripMenuItem.Text = "Zip";
+            this.archiveToolStripMenuItem.Click += new System.EventHandler(this.archiveToolStripMenuItem_Click);
+            // 
+            // unzipToolStripMenuItem
+            // 
+            this.unzipToolStripMenuItem.Name = "unzipToolStripMenuItem";
+            this.unzipToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.unzipToolStripMenuItem.Text = "Unzip";
+            this.unzipToolStripMenuItem.Click += new System.EventHandler(this.unzipToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Padding = new System.Windows.Forms.Padding(0);
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 26);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Padding = new System.Windows.Forms.Padding(0);
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(45, 26);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Padding = new System.Windows.Forms.Padding(0);
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(45, 26);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(37, 26);
+            this.toolStripMenuItem2.Text = "->";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(37, 26);
+            this.toolStripMenuItem3.Text = "<-";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 446);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem archiveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unzipToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ FileManager/FileManager/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        string LastFilename { get; set; } = "";
+        string LastZipname { get; set; } = "";
+        bool IsSaved { get; set; } = true;
+        int LMaxSize { get; } = 100;
+        LinkedList<Data> back;
+        LinkedList<Data> front;
+
+        public Form1(string text)
+        {
+            InitializeComponent();
+
+            textBox1.Text = text;
+            Text = "untitled.txt";
+
+            back = new LinkedList<Data>();
+            front = new LinkedList<Data>();
+            back.AddLast(new Data(text, GetPos()));
+        }
+
+        private void SaveTxt(string filename)
+        {
+            File.WriteAllText(filename, textBox1.Text);
+        }
+        private void SaveDat(string filename)
+        {
+            BinaryWriter writer = new BinaryWriter(File.Open(filename, FileMode.OpenOrCreate));
+            writer.Write(textBox1.Text);
+            writer.Close();
+        }
+        private void OpenTxt(string filename)
+        {
+            textBox1.Text = File.ReadAllText(filename);
+        }
+        private void OpenDat(string filename)
+        {
+            BinaryReader reader = new BinaryReader(File.Open(filename, FileMode.Open));
+            textBox1.Text = reader.ReadString();
+            reader.Close();
+        }
+        private void GlobalOpen()
+        {
+            if (SaveIsChecked())
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Text files(*.txt)|*.txt|Binary files(*.dat)|*.dat";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Text = Path.GetFileName(dialog.FileName);
+                    LastFilename = dialog.FileName;
+                    LastZipname = "";
+                    if (Path.GetExtension(LastFilename) == ".txt")
+                        OpenTxt(LastFilename);
+                    if (Path.GetExtension(LastFilename) == ".dat")
+                        OpenDat(LastFilename);
+                    IsSaved = true;
+                    back.Clear();
+                    front.Clear();
+                    back.AddLast(new Data(textBox1.Text, GetPos()));
+                }
+            }
+        }
+        private void GlobalSave()
+        {
+            if (LastFilename != "")
+            {
+                if (Path.GetExtension(LastFilename) == ".txt")
+                    SaveTxt(LastFilename);
+                if (Path.GetExtension(LastFilename) == ".dat")
+                    SaveDat(LastFilename);
+                IsSaved = true;
+            }
+            else
+            {
+                GlobalSaveAs();
+            }
+        }
+        private void GlobalSaveAs()
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text files(*.txt)|*.txt|Binary files(*.dat)|*.dat";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Text = Path.GetFileName(dialog.FileName);
+                LastFilename = dialog.FileName;
+                if (dialog.FilterIndex == 1)
+                    SaveTxt(LastFilename);
+                else
+                {
+                    SaveDat(LastFilename);
+                }
+                IsSaved = true;
+            }
+        }
+        private void GlobalNew()
+        {
+            if (SaveIsChecked())
+            {
+                Text = "untitled.txt";
+                textBox1.Text = "";
+                LastFilename = "";
+                LastZipname = "";
+                IsSaved = true;
+                back.Clear();
+                front.Clear();
+                back.AddLast(new Data(textBox1.Text, GetPos()));
+            }
+        }
+        private void Zip()
+        {
+            using (FileStream zipToOpen = new FileStream(LastZipname, FileMode.Open))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    ZipArchiveEntry readmeEntry = archive.CreateEntry(Text);
+                    using (StreamWriter writer = new StreamWriter(readmeEntry.Open()))
+                    {
+                        writer.Write(textBox1.Text);
+                    }
+                }
+            }
+        }
+        private void GlobalZip()
+        {
+            if (LastZipname != "")
+            {
+                Zip();
+            }
+            else
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Archive files(*.zip)|*.zip";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    LastZipname = dialog.FileName;
+                    using (FileStream zipFile = File.Open(LastZipname, FileMode.Create)) { } ;
+                    Zip();
+                }
+            }
+        }
+        private void GlobalUnzip()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Archive files(*.zip)|*.zip";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                LastZipname = dialog.FileName;
+                using (ZipArchive archive = ZipFile.Open(LastZipname, ZipArchiveMode.Update))
+                {
+                    int counter = 0;
+                    foreach(ZipArchiveEntry entry in archive.Entries)
+                    {
+                        if(Path.GetExtension(entry.FullName) == ".txt" || Path.GetExtension(entry.FullName) == ".dat")
+                            using (StreamReader reader = new StreamReader(entry.Open()))
+                            {
+                                if (counter == 0)
+                                {
+                                    GlobalNew();
+                                    textBox1.Text = reader.ReadToEnd();
+                                }
+                                else
+                                {
+                                    Thread thread = new Thread(new ParameterizedThreadStart(SummonForm));//
+                                    thread.SetApartmentState(ApartmentState.STA);
+                                    thread.Start(reader.ReadToEnd());
+                                }
+                                counter++;
+                                entry.LastWriteTime = DateTimeOffset.UtcNow.LocalDateTime;
+                            }                      
+                    }
+                    if (counter == 0)
+                    {
+                        MessageBox.Show("Txt or dat files are not found", "Notepad", MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+        private void SummonForm(object text)
+        {
+            Form1 form = new Form1(text.ToString());
+            Application.Run(form);
+        }
+        private bool SaveIsChecked()
+        {
+            if(!IsSaved)
+            {
+                DialogResult res = MessageBox.Show("Do you want to save current file?", "Notepad", MessageBoxButtons.YesNoCancel);
+                if(res == DialogResult.Yes)
+                {
+                    GlobalSave();
+                    if (IsSaved)
+                        return true;
+                    else
+                        return false;
+                }
+                else if(res == DialogResult.No)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GlobalSave();
+        }
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GlobalSaveAs();
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GlobalOpen();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            IsSaved = false;
+
+            var temp = new Data(textBox1.Text, GetPos());
+            if (temp != back.Last.Value)
+            {
+                back.AddLast(temp);
+                if (back.Count == LMaxSize + 1)
+                    back.RemoveFirst();
+            }
+        }
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GlobalNew();
+        }
+        private void archiveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GlobalZip();
+        }
+        private void unzipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GlobalUnzip();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            if(back.Count > 1)
+            {
+                front.AddFirst(back.Last.Value);
+                back.RemoveLast();
+                textBox1.Text = back.Last.Value.Text;
+                moveAt(back.Last.Value.CarP);
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if(front.Count != 0)
+            {
+                back.AddLast(front.First.Value);
+                front.RemoveFirst();
+                textBox1.Text = back.Last.Value.Text;
+                moveAt(back.Last.Value.CarP);
+            }
+        }
+
+        private void moveAt(int charsFromStart)
+        {
+            textBox1.Select(charsFromStart, 0);
+            textBox1.Focus();
+        }
+
+        private int GetPos()
+        {
+            int res = 0;
+            foreach(string line in textBox1.Lines)
+            {
+                res += line.Length + 2;
+            }
+            return res;
+        }
+    }
+
+    public class Data
+    {
+        public Data(string text, int carP) { Text = text; CarP = carP; }
+
+        public string Text { get; set; }
+        public int CarP { get; set; }
+
+        public static bool operator ==(Data a, Data b)
+        {
+            return a.Text == b.Text && a.CarP == b.CarP;
+        }
+
+        public static bool operator !=(Data a, Data b)
+        {
+            return !(a==b);
+        }
+    }
+}
